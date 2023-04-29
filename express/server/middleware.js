11ef439855169f94d43b5893acb68d291239ad46
MeshachArinze <meshacharinze@gmail.com>
2023-04-29 02:04:11 +0100

    commit to repo

--- express/server/middleware.js
+++ express/server/middleware.js
@@ -5,7 +5,7 @@ const hostname = "127.0.0.1";
 const port = 3000;
 
 const logger = (req, res, next) => {
-    const method = req.methodconst 
+    const method = req.method
     const url = req.url
     const time = new Date().getFullYear()
     console.log(method, url, time)
