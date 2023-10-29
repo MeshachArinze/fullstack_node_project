7934f2432879f6386ecac40edec1d528b01bbe51
MeshachArinze <meshacharinze@gmail.com>
2023-10-29 12:39:14 +0100

    commit to main

--- fundamental/globals.js
+++
@@ -1,9 +0,0 @@
-const ac = new AbortController();
-
-ac.signal.addEventListener("abort", () => console.log("Aborted!"), {
-  once: true,
-});
-
-ac.abort();
-
-console.log(ac.signal.aborted); // Prints True
--- fundamental/intro.js
+++
@@ -1,7 +0,0 @@
-const os =require('node:os')
-
-// check current user
-
-const user = os.userInfo()
-const cpus = os.cpus();
-console.log(user, cpus)
