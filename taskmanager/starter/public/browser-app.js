c9886d228b22ff2b88fe71c80e7969d23fb2b3cb
MeshachArinze <meshacharinze@gmail.com>
2023-10-29 12:49:35 +0100

    commit to main

---
+++ taskmanager/starter/public/browser-app.js
@@ -0,0 +1,5 @@
+const tasksDOM = document.querySelector(".tasks"),
+  loadingDOM = document.querySelector(".loading-text"),
+  formDOM = document.querySelector(".task-form"),
+  taskInputDOM = document.querySelector(".task-input"),
+  formAlertDOM = document.querySelector(".form-alert");
