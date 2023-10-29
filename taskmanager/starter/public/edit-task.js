3415628f96930c05f5b0cb76e4a6f8fb6ee1fa4b
MeshachArinze <meshacharinze@gmail.com>
2023-10-29 13:27:26 +0100

    commit to main

---
+++ taskmanager/starter/public/edit-task.js
@@ -0,0 +1,8 @@
+const taskIDDOM = document.querySelector(".task-edit-id");
+const taskNameDOM = document.querySelector(".task-edit-name");
+const taskCompletedDOM = document.querySelector(".task-edit-completed");
+const editFormDOM = document.querySelector(".single-task-form");
+const editBtnDOM = document.querySelector(".task-edit-btn");
+const formAlertDOM = document.querySelector(".form-alert");
+const params = window.location.search;
+const id = new URLSearchParams(params).get("id");
