0ab5169fb79c0dbe654a372bbb800ea13ae61a89
MeshachArinze <meshacharinze@gmail.com>
2023-10-29 13:10:09 +0100

    commit to main

--- taskmanager/starter/public/browser-app.js
+++ taskmanager/starter/public/browser-app.js
@@ -1,5 +1,52 @@
+import axios from "axios";
+
 const tasksDOM = document.querySelector(".tasks"),
   loadingDOM = document.querySelector(".loading-text"),
   formDOM = document.querySelector(".task-form"),
   taskInputDOM = document.querySelector(".task-input"),
   formAlertDOM = document.querySelector(".form-alert");
+
+// load tasks from /api/tasks
+
+const showTask = async () => {
+  loadingDOM.style.visibility = "visible";
+
+  try {
+    const {
+      data: { tasks },
+    } = await axios.get("/api/v1/tasks");
+
+    if (tasks.length < 1) {
+      tasksDOM.innerHTML = '<h5 class="empty-list">No tasks in your list</h5>';
+
+      loadingDOM.style.visibility = "hidden";
+      return;
+    }
+
+    const allTasks = tasks.map((task) => {
+      const { completed, _id: taskID, name } = task;
+      return `<div class="single-task ${completed && "task-completed"}">
+<h5><span><i class="far fa-check-circle"></i></span>${name}</h5>
+<div class="task-links">
+      
+<!-- edit link -->
+<a href="task.html?id=${taskID}"  class="edit-link">
+<i class="fas fa-edit"></i>
+</a>
+<!-- delete btn -->
+<button type="button" class="delete-btn" data-id="${taskID}">
+<i class="fas fa-trash"></i>
+</button>
+</div>
+</div>`;
+    }).join('');
+    tasksDOM.innerHTML = allTasks;
+  } catch (error) {
+    tasksDOM.innerHTML =
+      '<h5 class="empty-list">There was an error, please try later....</h5>';
+  }
+
+  loadingDOM.style.visibility = 'hidden'
+};
+
+showTask();
