c0ab0985d6788ece8fb72c7c1f2dcfa02306fb26
MeshachArinze <meshacharinze@gmail.com>
2023-10-29 13:23:34 +0100

    commit to main

--- taskmanager/starter/public/browser-app.js
+++ taskmanager/starter/public/browser-app.js
@@ -83,6 +83,12 @@ formDOM.addEventListener('submit', async (e) => {
         formAlertDOM.textContent = `success, task added`;
         formAlertDOM.classList.add("text-success");
     } catch (error) {
-        
+            formAlertDOM.style.display = "block";
+            formAlertDOM.innerHTML = `error, please try again`;
     }
+
+    setTimeout(() => {
+        formAlertDOM.style.display = `none`
+        formAlertDOM.classList.remove("text-success");
+    }, 3000);
 })
